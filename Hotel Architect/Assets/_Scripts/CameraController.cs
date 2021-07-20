660f6d9281bafaacec96d3f36d97a91a3abd3bbc
Adam Harris <adam_harris@hotmail.com>
2021-07-20 21:26:27 +0100

    Improved camera controls

--- Hotel Architect/Assets/_Scripts/CameraController.cs
+++ Hotel Architect/Assets/_Scripts/CameraController.cs
@@ -2,28 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraController : MonoBehaviour
 {
     [SerializeField] float _rotationSpeed = 15.0f;
     [SerializeField] float _movementSpeed = 5.0f;
+    [SerializeField] float _zoomInSpeed = 3.0f;
+    [SerializeField] float _pitchingSpeed = 15.0f;
+
+
+    Cinemachine.CinemachineVirtualCamera _virtualCamera;
+    Cinemachine.CinemachineFramingTransposer _transponder;
+
+    void Awake()
+    {
+        _virtualCamera = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
+        _transponder = _virtualCamera.GetCinemachineComponent<Cinemachine.CinemachineFramingTransposer>();
+
+
+        if(_transponder == null) Debug.LogError("_transponder is null");
+        
+        
+    }
 
     void Update()
     {
+        //Moving the camera
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        if (movement != Vector3.zero) transform.Translate(movement * _movementSpeed * Time.deltaTime);
 
-        if (movement != Vector3.zero)
-        {
-            transform.Translate(movement * _movementSpeed * Time.deltaTime);
-        }
+        //Rotating the Camera
+        if (Input.GetKey(KeyCode.Q)) transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
+        else if(Input.GetKey(KeyCode.E)) transform.Rotate(Vector3.up, -_rotationSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(Vector3.up, -_rotationSpeed * Time.deltaTime);
-        }
-        else if(Input.GetKey(KeyCode.E))
-        {
-            transform.Rotate(Vector3.up, _rotationSpeed * Time.deltaTime);
-        }
+        //Zoom in controls.
+        Vector2 scroll = Input.mouseScrollDelta;
+        scroll *= -1.0f;
+
+        float camDistance = _transponder.m_CameraDistance;
+        camDistance = Mathf.Clamp(camDistance + scroll.y * _zoomInSpeed * Time.deltaTime, 0.01f, 100.0f);
+        _transponder.m_CameraDistance = camDistance;
+
+        //"Pitch" into scene
+        Vector3 trackedOffset = _transponder.m_TrackedObjectOffset;
+        if (Input.GetKey(KeyCode.R)) trackedOffset.y = Mathf.Clamp(trackedOffset.y + -_pitchingSpeed * Time.deltaTime, 2.5f, 25.0f);
+        else if (Input.GetKey(KeyCode.F)) trackedOffset.y = Mathf.Clamp(trackedOffset.y + _pitchingSpeed * Time.deltaTime, 2.5f, 25.0f);
+        _transponder.m_TrackedObjectOffset = trackedOffset;
+
+
 
     }
 }
--- Hotel Architect/Assets/_Scripts/ObjectPlacer.cs
+++ Hotel Architect/Assets/_Scripts/ObjectPlacer.cs
@@ -19,26 +19,24 @@ public class ObjectPlacer : MonoBehaviour
     {
         _placerPreview = Instantiate(_previewObject, Vector3.zero, Quaternion.identity, transform);
         _placeableLayerNum = LayerMask.NameToLayer("Placeable");
-        Debug.Log(_placeableLayerNum);
     }
 
     private void Update()
     {
         SnapPreviewToGrid();
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if(!Physics.CheckBox(_currentPoint, Vector3.one / 3, Quaternion.identity))
-            {
+            if (!Physics.CheckBox(_currentPoint, Vector3.one / 3, Quaternion.identity))
                 Instantiate(_placeObject, _currentPoint, Quaternion.identity, transform);
-            }
-
-            if(!Physics.Raycast(ray, out hit, 100.0f, _placeableMask))
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f, _placeableMask))
             {
-                
+                Destroy(hit.transform.gameObject);
             }
         }
     }
@@ -52,23 +50,22 @@ public class ObjectPlacer : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 100.0f, _interactableMask))
         {
-            _placerPreview.SetActive(true);
-
             _currentPoint = hit.point;
-
-
 
             if (hit.transform.gameObject.layer == _placeableLayerNum)
             {
                 _currentPoint += hit.normal / 2;
+                _placerPreview.SetActive(false);
             }
+            else _placerPreview.SetActive(true);
 
             _currentPoint.x = Mathf.Round(_currentPoint.x);
             _currentPoint.y = 0.5f;
             _currentPoint.z = Mathf.Round(_currentPoint.z);
 
             _placerPreview.transform.position = _currentPoint;
-        }else
+        }
+        else
         {
             _placerPreview.SetActive(false);
         }
