c21f3495ea74267544398ae8a07a35e846da5aeb
Adam Harris <adam_harris@hotmail.com>
2021-07-21 14:39:36 +0100

    Objects can be rotated when placed

--- Hotel Architect/Assets/_Scripts/ObjectPlacer.cs
+++ Hotel Architect/Assets/_Scripts/ObjectPlacer.cs
@@ -39,38 +39,29 @@ public class ObjectPlacer : MonoBehaviour
         _mainCam = Camera.main;
     }
 
-    private void Update()
+    private void InteractWithMouse()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeObject(_fullWall);
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeObject(_halfWall);
-        else if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeObject(_sofa);
-        else if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeObject(_largeSofa);
-        
-        if(Input.GetKeyDown(KeyCode.X))
-        {
-            _isRotatated = !_isRotatated;
-
-            if (_isRotatated) _placerPreview.transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
-            else _placerPreview.transform.Rotate(new Vector3(0.0f, -90.0f, 0.0f));
-        }
-
-        SnapPreviewToGrid();
-
-
-        Vector3 _buildPoint = _currentPoint;
-        _buildPoint.y += _placeObject._config._sizeInMetres.y / 2.0f;
-
-        Vector3 extents = _placeObject._config._sizeInMetres / 2.0f;
-        extents.x -= 0.02f;
-        extents.y -= 0.02f;
-        extents.z -= 0.02f;
-        //Vector3 extents = Vector3.one / 3.0f;
-
         if (Input.GetMouseButtonDown(0))
         {
+            Vector3 _buildPoint = _currentPoint;
+            _buildPoint.y += _placeObject._config._sizeInMetres.y / 2.0f;
+
+            Vector3 extents = _placeObject._config._sizeInMetres / 2.0f;
+            extents.x -= 0.02f;
+            extents.y -= 0.02f;
+            extents.z -= 0.02f;
+
+            
+
             //Check that there isnt an object occupying this space.
             if (!Physics.CheckBox(_buildPoint, extents))
-                Instantiate(_placeObject, _buildPoint, Quaternion.identity, transform);
+            {
+                Quaternion objectRotation = Quaternion.identity;
+                if (_isRotatated) objectRotation.eulerAngles = new Vector3(0.0f, 90.0f, 0.0f);
+
+                Instantiate(_placeObject, _buildPoint, objectRotation, transform);
+            }
+                
         }
         else if (Input.GetMouseButtonDown(1))
         {
@@ -81,6 +72,31 @@ public class ObjectPlacer : MonoBehaviour
                 Destroy(hit.transform.gameObject);
             }
         }
+    }
+
+    private void InteractWithKeyboard()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeObject(_fullWall);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeObject(_halfWall);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeObject(_sofa);
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeObject(_largeSofa);
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            _isRotatated = !_isRotatated;
+
+            if (_isRotatated) _placerPreview.transform.Rotate(new Vector3(0.0f, 90.0f, 0.0f));
+            else _placerPreview.transform.Rotate(new Vector3(0.0f, -90.0f, 0.0f));
+        }
+    }
+
+    private void Update()
+    {
+        InteractWithKeyboard();
+
+        SnapPreviewToGrid();
+
+        InteractWithMouse();
     }
 
     void SnapPreviewToGrid()
